9437315750c802664b01a24e612ef48aa14c9c6e
Nikhil-L <nikhil89515@gmail.com>
2019-11-03 10:51:26 +0530

    Include the homepage url and its views in url file and templates folder in the settings

--- Online_Shopping_System/settings.py
+++ Online_Shopping_System/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'Online_Shopping_System.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
