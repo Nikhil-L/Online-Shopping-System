d1f084d8ceaef462d9bc200126d5250b0652f825
Nikhil-L <nikhil89515@gmail.com>
2019-11-05 17:40:41 +0530

    Remove error caused in models.py due to Capital 'C' in class

--- products/models.py
+++ products/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-Class Product(models.Model):
+class Product(models.Model):
     name = models.CharField(max_length = 100)
     slug = models.SlugField()
     detail = models.TextField()
