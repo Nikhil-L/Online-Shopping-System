56dfe4c6e0ff7971e6c84d4b9a48bcb89dcec938
Nikhil-L <nikhil89515@gmail.com>
2019-11-06 21:19:02 +0530

    Return the name of the product instead of some class value during objects retrieval

--- products/models.py
+++ products/models.py
@@ -6,3 +6,6 @@ class Product(models.Model):
     name = models.CharField(max_length = 100)
     slug = models.SlugField()
     detail = models.TextField()
+
+    def __str__(self):
+        return self.name
