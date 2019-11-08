a1af74c3db92ff2a42cad6ec9ff059fca124bdc2
Nikhil-L <nikhil89515@gmail.com>
2019-11-08 20:13:55 +0530

    Add the code in views.py to pass the list of products to the products template

--- products/views.py
+++ products/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
 def product_list(request):
-    return render(request, 'products/product_list.html')
+    products = Product.objects.all()
+    return render(request, 'products/product_list.html', {'products' : products})
