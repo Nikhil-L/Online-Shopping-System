76b35af1dfb77b77cd937ce19a88cc1f8fb33b14
Nikhil-L <nikhil89515@gmail.com>
2019-11-08 20:35:56 +0530

    Rename the detail field as details in product

--- products/models.py
+++ products/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Product(models.Model):
     name = models.CharField(max_length = 100)
     slug = models.SlugField()
-    detail = models.TextField()
+    details = models.TextField()
 
     def __str__(self):
         return self.name
