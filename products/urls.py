1db1503ee5537500bedc85fca8fed10fd0f872f6
Nikhil-L <nikhil89515@gmail.com>
2019-11-09 20:26:53 +0530

    Display the details of individual items in separate webpage

--- products/urls.py
+++ products/urls.py
@@ -2,6 +2,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'products'
+
 urlpatterns = [
-    path('', views.product_list, name = 'product_list')
+    path('', views.product_list, name = 'product_list'),
+    path('<slug:slug>/', views.product_details, name = 'details')
 ]
--- products/views.py
+++ products/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from .models import Product
+from django.http import HttpResponse
 
 # Create your views here.
 def product_list(request):
     products = Product.objects.all()
     return render(request, 'products/product_list.html', {'products' : products})
+
+def product_details(request, slug):
+    #return HttpResponse(slug)
+    product = Product.objects.get(slug = slug)
+    return render(request, 'products/product_details.html', {'product':product})
