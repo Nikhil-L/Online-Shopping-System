ffcb100423350f831bbaf2f909b72c6039e14efe
Nikhil-L <nikhil89515@gmail.com>
2019-11-10 19:58:32 +0530

    Add signup, login and logout urls and views

--- Online_Shopping_System/urls.py
+++ Online_Shopping_System/urls.py
@@ -9,6 +9,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('products/', include('products.urls')),
+    path('accounts/', include('accounts.urls')),
     path('', views.homepage, name = 'home')
 ]
 
