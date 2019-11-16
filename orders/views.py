f4322bd16003aa61a7cbf2d4b02fdf7c2898d6bb
Nikhil-L <nikhil89515@gmail.com>
2019-11-16 10:53:09 +0530

    Redirect users who are not logged in to login page while trying to view the order deatails page

--- orders/views.py
+++ orders/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
+
+@login_required(login_url = "/accounts/login/")
 def order_details(request):
     return HttpResponse("orders")
