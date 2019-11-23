dcb90dc37a2645ec6d1120097af967b7b82524d1
Nikhil-L <nikhil89515@gmail.com>
2019-11-23 19:43:58 +0530

    Add username to the customer details as one of the input field which is filled by the server itself

--- customer/models.py
+++ customer/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Customer(models.Model):
     FirstName = models.CharField(max_length = 30)
     LastName = models.CharField(max_length = 30)
+    UserName = models.ForeignKey(User,default = 'None', blank = True, on_delete = models.CASCADE)
     Email = models.EmailField(max_length = 100)
     Phone = models.BigIntegerField()
     Address = models.CharField(max_length = 250)
