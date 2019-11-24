70104c60804bf9e8828c66c587acf68d07e9e37f
Nikhil-L <nikhil89515@gmail.com>
2019-11-24 13:33:03 +0530

    Update stock details and display appropriate msg when order is out of stock

--- orders/views.py
+++ orders/views.py
@@ -10,17 +10,23 @@ from django.contrib.auth.models import User
 @login_required(login_url = "/accounts/login/")
 def order_details(request):
     if request.method == 'POST':
-        customer = Customer.objects.get(UserName = request.user)
         product = Product.objects.get(id = request.POST.get('id'))
-        order = Orders( username = request.user,
-                        productname = product.name,
-                        cost = product.cost,
-                        customer_address = customer.Address,
-                        city = customer.City,
-                        pincode = customer.Pincode,
-                        phone = customer.Phone)
-        order.save()
-        return redirect('home')
+        if product.items_remaining >= 1:
+            customer = Customer.objects.get(UserName = request.user)
+
+            order = Orders( username = request.user,
+                            productname = product.name,
+                            cost = product.cost,
+                            customer_address = customer.Address,
+                            city = customer.City,
+                            pincode = customer.Pincode,
+                            phone = customer.Phone)
+            order.save()
+            product.items_remaining = product.items_remaining -1
+            product.save()
+            return redirect('home')
+        else:
+            return HttpResponse("Out of Stock")
 
     else:
         orders = Orders.objects.filter(username = request.user)
